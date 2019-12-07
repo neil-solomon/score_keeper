7d0b31cbce79a9647aa4eb06026555600b274ddf
Neil-Solomon <NeilSolomon89@gmail.com>
2019-12-07 16:06:24 -0500

    Updates

---
+++ precache-manifest.eb8401b7123f59495f4f789a4ca11e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258e85b365c5dcf85076e871b9fac44e",
+    "url": "/score_keeper/index.html"
+  },
+  {
+    "revision": "d77a80fd575aa974a316",
+    "url": "/score_keeper/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "6ea5f929a45d8be00f0b",
+    "url": "/score_keeper/static/css/main.3c397f6e.chunk.css"
+  },
+  {
+    "revision": "d77a80fd575aa974a316",
+    "url": "/score_keeper/static/js/2.a1c5c76d.chunk.js"
+  },
+  {
+    "revision": "6ea5f929a45d8be00f0b",
+    "url": "/score_keeper/static/js/main.62375f08.chunk.js"
+  },
+  {
+    "revision": "dc881cdd8c3d599f43a3",
+    "url": "/score_keeper/static/js/runtime-main.f0f57d56.js"
+  }
+]);
